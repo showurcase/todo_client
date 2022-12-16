47b95abff2bb8c58a83a60d25b9397b02b7fe774
Denis Chernoskutov <lolmumdno@gmail.com>
2022-12-16 23:21:21 +0800

    commit3

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,25 +3,25 @@ import axios from "axios";
 
 export class TodoApi {
     static async getTodos(): Promise<ITodoState[]> {
-        const res = await axios.get('http://localhost:8080/todos')
+        const res = await axios.get('http://45.12.18.2:8080/todos')
         return res.data
     }
 
     static async createTodo(todo: Partial<ITodo>): Promise<ITodoState[]> {
-        const res = await axios.post('http://localhost:8080/todos', todo)
+        const res = await axios.post('http://45.12.18.2:8080/todos', todo)
         return res.data
     }
 
     static async deleteTodo(id: string): Promise<void> {
-        await axios.delete(`http://localhost:8080/todos/${id}`)
+        await axios.delete(`http://45.12.18.2:8080/todos/${id}`)
     }
 
     static async completeTodo(todo: Partial<ITodo>): Promise<void> {
-        await axios.patch(`http://localhost:8080/todos/${todo.id}`, todo)
+        await axios.patch(`http://45.12.18.2:8080/todos/${todo.id}`, todo)
     }
 
     static async editTodo(todo: Partial<ITodo>): Promise<ITodo> {
-        const res = await axios.patch(`http://localhost:8080/todos/${todo.id}`, todo)
+        const res = await axios.patch(`http://45.12.18.2:8080/todos/${todo.id}`, todo)
         return res.data[1][0]
     }
 
